54ecc2dae4d148c3cd717b81b98a9890ecf7d0c0
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-07 11:35:22 -0300

    recursive factorial

---
+++ debugging/factorial_recursive.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+	if n == 0:
+		return 1
+	else:
+		return n * factorial(n-1)
+
+if len(sys.argv) != 2:
+    print("Usage: ./factorial_recursive.py <number>")
+    sys.exit(1)
+
+try:
+    f = factorial(int(sys.argv[1]))
+    print(f)
+except ValueError:
+    print("Please enter a valid integer.")
