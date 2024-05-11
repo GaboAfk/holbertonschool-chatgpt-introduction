c82cfea7583d600f5ce74f4d0fc9dc11f7e615aa
GaboAfk <gabrielmorffe95@gmail.com>
2024-05-11 19:43:03 -0300

    recursive factorial

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -2,10 +2,10 @@
 import sys
 
 def factorial(n):
-	if n == 0:
-		return 1
-	else:
-		return n * factorial(n-1)
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
 
 if len(sys.argv) != 2:
     print("Usage: ./factorial_recursive.py <number>")
